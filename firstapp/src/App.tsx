71e9dc96e05ad37424a58f814858b69b800c36ce
MinaSameh1 <minasameh1@gmail.com>
2023-01-09 21:44:03 +0200

    Set basic app look.

--- firstapp/src/App.tsx
+++ firstapp/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
+  FlatList,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -11,20 +13,57 @@ import { colors } from './config'
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
+  const [goals, setGoals] = useState<Array<Record<string, string>>>([])
+  const [goalEntered, setGoalEntered] = useState('')
+
+  function inputHandler(txt: string) {
+    setGoalEntered(txt)
+  }
+
+  function addGoalHandler() {
+    if (goalEntered === '') return
+    setGoals(currentGoals => [
+      ...currentGoals,
+      { text: goalEntered, _id: Math.random().toString() }
+    ])
+  }
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? colors.dark : colors.light,
-    color: isDarkMode ? colors.light : colors.dark
+    color: isDarkMode ? colors.light : colors.dark,
+    borderBottomColor: isDarkMode ? colors.lightpink : colors.dark
   }
 
   return (
     <View style={{ ...styles.container, ...backgroundStyle }}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.inputText} placeholder="Write here" />
-        <Button title="add item" />
+      <View
+        style={{
+          ...styles.inputContainer,
+          borderBottomColor: backgroundStyle.borderBottomColor
+        }}>
+        <TextInput
+          style={{ ...styles.inputText, color: backgroundStyle.color }}
+          placeholder="Write your goal here"
+          onChangeText={inputHandler}
+        />
+        <Button title="add item" onPress={addGoalHandler} />
       </View>
       <View style={styles.listContainer}>
-        <Text>List of items</Text>
+        <FlatList
+          data={goals}
+          renderItem={Items => (
+            <View
+              style={{
+                ...styles.goalItem,
+                backgroundColor: isDarkMode ? colors.purple : colors.lightpurple
+              }}>
+              <Text style={{ color: backgroundStyle.color }}>
+                {Items.item.text}
+              </Text>
+            </View>
+          )}
+          keyExtractor={(item, _index) => item._id}
+        />
       </View>
     </View>
   )
@@ -32,21 +71,34 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 10,
     paddingTop: 20
   },
   listContainer: {
+    flex: 10,
     padding: 20
   },
   inputText: {
     borderWidth: 1,
-    borderColor: '#0232a2',
-    width: '80%',
+    borderColor: colors.blue,
+    width: '70%',
     marginRight: 8
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingBottom: 20,
+    borderBottomWidth: 1
+  },
+  goalItem: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: colors.purple
   }
 })
 
--- firstapp/src/config.ts
+++ firstapp/src/config.ts
@@ -1,4 +1,8 @@
 export const colors = {
   dark: '#000',
-  light: '#fff'
+  light: '#fff',
+  blue: '#0232a2',
+  lightpink: '#ca9fcf',
+  purple: '#5e0acc',
+  lightpurple: '#6f6fc0'
 }
