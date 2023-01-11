1a788b35caa96431c98a58aa75d491a0be30668e
MinaSameh1 <minasameh1@gmail.com>
2023-01-11 21:59:01 +0200

    Added navigation and basic setup

--- navigation/src/components/Menu.tsx
+++ navigation/src/components/Menu.tsx
@@ -1,15 +1,31 @@
-import { View, StyleSheet, Button, Pressable, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { View, StyleSheet, Pressable, Text } from 'react-native'
+import { RootStackParams } from '../main'
 
 export const Menu: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParams>>()
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button} onPress={() => { }}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('Explore')
+        }}>
         <Text style={styles.text}>Explore</Text>
       </Pressable>
-      <Pressable style={styles.button} onPress={() => { }}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('Items')
+        }}>
         <Text style={styles.text}>Items</Text>
       </Pressable>
-      <Pressable style={styles.button} onPress={() => { }}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('Profile')
+        }}>
         <Text style={styles.text}>Profile</Text>
       </Pressable>
     </View>
--- navigation/src/main.tsx
+++ navigation/src/main.tsx
@@ -1,7 +1,27 @@
-import ItemsScreen from "./screens/Items"
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import ExploreScreen from './screens/Explore'
+import ItemsScreen from './screens/Items'
+import Profile from './screens/Profile'
+
+export type RootStackParams = {
+  Explore: undefined
+  Items: undefined
+  Profile: undefined
+}
+
+const RootStack = createNativeStackNavigator<RootStackParams>()
 
 const App = () => {
-  return <ItemsScreen />
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Explore" component={ExploreScreen} />
+        <RootStack.Screen name="Items" component={ItemsScreen} />
+        <RootStack.Screen name="Profile" component={Profile} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default App
