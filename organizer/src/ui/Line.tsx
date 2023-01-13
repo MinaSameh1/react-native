3ad551ba99d74d92a66716cae6c29a11fbcccee8
MinaSameh1 <minasameh1@gmail.com>
2023-01-13 19:20:08 +0200

    changed Line color

--- organizer/src/ui/Line.tsx
+++ organizer/src/ui/Line.tsx
@@ -1,11 +1,12 @@
-import { View } from 'native-base'
+import { useColorModeValue, View } from 'native-base'
 import Svg, { Path } from 'react-native-svg'
 
 export const Line = () => {
+  const color = useColorModeValue('black', 'white')
   return (
     <View>
-      <Svg width="150" height="1" viewBox="0 0 145 1" fill="none">
-        <Path opacity="0.1" d="M0.5 0.5H144.5" stroke="white" />
+      <Svg width="150" height="25" viewBox="0 0 145 1" fill="none">
+        <Path opacity="0.6" d="M0.5 0.5H144.5" stroke={color} />
       </Svg>
     </View>
   )
