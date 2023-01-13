21bfd3a8dfdee1c26278a5fe7937fa2f59af6aa0
MinaSameh1 <minasameh1@gmail.com>
2023-01-13 13:01:18 +0200

    misc refactoring

--- navigation/src/common/hooks/theme.hook.ts
+++ navigation/src/common/hooks/theme.hook.ts
@@ -2,7 +2,4 @@ import { useContext } from 'react'
 import { theme } from '../../config/theme.config'
 import ThemeContext from '../context/theme.context'
 
-export const useTheme = () => {
-  const CustomTheme = useContext(ThemeContext) as typeof theme.light
-  return CustomTheme
-}
+export const useTheme = () => useContext(ThemeContext) as typeof theme.light
