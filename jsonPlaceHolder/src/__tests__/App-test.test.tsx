554c991ae8a3ca1f8e5f2f06c982e81a5bc1b0d2
MinaSameh1 <minasameh1@gmail.com>
2023-01-31 23:54:10 +0200

    Misc cleanup

--- jsonPlaceHolder/src/__tests__/App-test.test.tsx
+++ jsonPlaceHolder/src/__tests__/App-test.test.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
-import { HomeScreen } from '../screen/HomeScreen'
+import { HomeScreen } from '../screens/HomeScreen'
 
 it('renders correctly', () => {
   renderer.create(<HomeScreen />)
