b816cfe5bde842c34ea06ea088051865435e88d5
Mina Sameh <mina.s.wadie@gmail.com>
2023-02-01 10:20:37 +0200

    Fixed Jest setup

--- jsonPlaceHolder/.eslintrc.js
+++ jsonPlaceHolder/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  root: true,
+  plugins: ['jest', 'prettier'],
   extends: ['@react-native-community', 'prettier']
 }
--- jsonPlaceHolder/jest.config.ts
+++ jsonPlaceHolder/jest.config.ts
@@ -1,18 +1,8 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 export default {
   preset: 'react-native',
-  testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['src/__tests__/e2e'],
-  transform: {
-    '^.+\\.(js)$': 'babel-jest',
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json'
-      }
-    ]
-  },
-  // setupFiles: ['<rootDir>/src/utils/setupTests.ts'],
+  setupFiles: ['<rootDir>/src/utils/jest.setup.ts'],
   testRegex: '(src/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js|jsx)$',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   cacheDirectory: '.jest/cache',
