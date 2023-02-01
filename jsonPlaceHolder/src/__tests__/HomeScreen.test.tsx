f8f60ea02c59664ca2e19bbfb760860d6f5f1655
Mina Sameh <mina.s.wadie@gmail.com>
2023-02-01 12:41:28 +0200

    Fixed detox

--- jsonPlaceHolder/src/__tests__/HomeScreen.test.tsx
+++ jsonPlaceHolder/src/__tests__/HomeScreen.test.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 import { HomeScreen } from '../screens/HomeScreen'
+import { Text } from 'react-native-paper'
 
 it('renders correctly', () => {
-  renderer.create(<HomeScreen />)
+  const testRend = renderer.create(<HomeScreen />)
+  const testInstance = testRend.root
+  expect(testInstance.findByType(Text).props.children).toEqual('this is home')
 })
