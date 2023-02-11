789de39cf83d61a99a5551c974417297ae09f933
MinaSameh1 <minasameh1@gmail.com>
2023-02-11 22:42:45 +0200

    Don't require react in scope

--- jsonPlaceHolder/.eslintrc.js
+++ jsonPlaceHolder/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   plugins: ['jest', 'prettier'],
-  extends: ['@react-native-community', 'prettier']
+  extends: ['@react-native-community', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react/react-in-jsx-scope': 'off'
+      }
+    }
+  ]
 }
